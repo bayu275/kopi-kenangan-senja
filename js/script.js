9c5bb5f37c152af88e99cc6b02fd2b2133b5e4d4
Bayu <bayuusetiawan86@gmail.com>
2024-01-14 13:16:44 +0700

    fix hamburger button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-const $ = (s) => document.querySelector(s);
+const $ = (s) => {
+    const elements = document.querySelectorAll(s);
+    return elements.length === 1 ? elements[0] : elements;
+};
 
-$('#hamburger-menu').addEventListener('click', function () {
+$('#hamburger-menu').addEventListener('click', function (e) {
     $('nav .navbar-nav').classList.toggle('active');
 });
 
@@ -8,4 +11,10 @@ document.addEventListener('click', function (e) {
     if (!e.target.matches('#hamburger-menu *, .navbar-nav *')) {
         $('nav .navbar-nav').classList.remove('active');
     }
+});
+
+$('.navbar-nav a').forEach((element) => {
+    element.addEventListener('click', function (e) {
+        $('nav .navbar-nav').classList.remove('active');
+    })
 });
