6a63f8cd0ab9dc299242580c3ff19e540cb359ed
Bayu <bayuusetiawan86@gmail.com>
2024-01-14 20:33:00 +0700

    update UI dan Javascript

--- js/script.js
+++ js/script.js
@@ -3,18 +3,49 @@ const $ = (s) => {
     return elements.length === 1 ? elements[0] : elements;
 };
 
+// Toggle Navbar
 $('#hamburger-menu').addEventListener('click', function (e) {
     $('nav .navbar-nav').classList.toggle('active');
 });
 
+// Toggle Search
+$('#search').addEventListener('click', function (e) {
+    $('nav .search-form').classList.toggle('active');
+    $('#search-box').focus();
+    e.preventDefault();
+});
+
+// Toggle Shopping Cart
+$('#shopping-cart').addEventListener('click', function (e) {
+    $('nav .shopping-cart').classList.toggle('active');
+    e.preventDefault();
+});
+
+$('.product-detail').forEach((element) => {
+    element.addEventListener('click', function (e) {
+        e.preventDefault();
+        $('#item-detail-modal').style.display = 'flex';
+    });
+});
+
+$('.modal-close').addEventListener('click', function (e) {
+    e.preventDefault();
+    $('#item-detail-modal').style.display = 'none';
+});
+
+// Close Navbar, Search & Shopping Cart
 document.addEventListener('click', function (e) {
     if (!e.target.matches('#hamburger-menu *, .navbar-nav *')) {
         $('nav .navbar-nav').classList.remove('active');
     }
-});
 
-$('.navbar-nav a').forEach((element) => {
-    element.addEventListener('click', function (e) {
-        $('nav .navbar-nav').classList.remove('active');
-    })
+    if (!e.target.matches('#search *, .search-form *')) {
+        $('nav .search-form').classList.remove('active');
+    }
+    if (!e.target.matches('#shopping-cart *, .shopping-cart *')) {
+        $('nav .shopping-cart').classList.remove('active');
+    }
+    if (e.target === $('#item-detail-modal')) {
+        $('#item-detail-modal').style.display = 'none';
+    }
 });
