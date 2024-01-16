753b240edd2f61b23d770d91d04b73d716c32fef
Bayu <bayuusetiawan86@gmail.com>
2024-01-16 23:19:43 +0700

    update shopping cart

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 import products from './data-seed.js';
-// import Navbar from '../views/Navbar.vue';
 
 const app = Vue.createApp({
     data() {
@@ -18,10 +17,49 @@ const app = Vue.createApp({
     },
     methods: {
         addToCart(product) {
-            this.cart.items.push(product);
-            this.cart.total += product.price;
-            this.cart.qty++;
-            console.log(this.cart.total);
+            const { id, price } = product;
+
+            const cartItem = this.cart.items.find((chunk) => chunk.id === id);
+
+            if (!cartItem) {
+                this.cart.items.push({ ...product, qty: 1, total: price });
+                this.cart.total += price;
+                this.cart.qty++;
+            } else {
+                this.cart.items = this.cart.items.map((chunk) => {
+                    if (chunk.id !== id) {
+                        return chunk;
+                    } else {
+                        chunk.qty++;
+                        chunk.total = chunk.price * chunk.qty;
+                        this.cart.qty++;
+                        this.cart.total += chunk.price;
+                        return chunk;
+                    }
+                });
+            }
+        },
+        removeFromCart(product) {
+            const { id } = product;
+            const cartItem = this.cart.items.find((chunk) => chunk.id === id);
+
+            if (cartItem.qty > 1) {
+                this.cart.items = this.cart.items.map((chunk) => {
+                    if (chunk.id !== id) {
+                        return chunk;
+                    } else {
+                        chunk.qty--;
+                        chunk.total = chunk.price * chunk.qty;
+                        this.cart.qty--;
+                        this.cart.total -= chunk.price;
+                        return chunk;
+                    }
+                });
+            } else {
+                this.cart.items = this.cart.items.filter((chunk) => chunk.id !== id);
+                this.cart.qty--;
+                this.cart.total -= cartItem.total;
+            }
         },
         formattedPrice(price) {
             const rupiah = new Intl.NumberFormat('id-ID', {
@@ -47,8 +85,7 @@ const app = Vue.createApp({
             this.showModal = false;
         },
         handleClickOutside(event) {
-            const { navbarNav, hamburgerMenu, btnCart, shoppingCart, searchForm, btnSearch, modalClose, modal } =
-                this.$refs;
+            const { navbarNav, hamburgerMenu, btnCart, shoppingCart, searchForm, btnSearch, modal } = this.$refs;
 
             if (this.showMenu && !navbarNav.contains(event.target) && !hamburgerMenu.contains(event.target)) {
                 this.showMenu = false;
@@ -73,55 +110,3 @@ const app = Vue.createApp({
     },
 });
 app.mount('#app');
-
-// const $ = (s) => {
-//     const elements = document.querySelectorAll(s);
-//     return elements.length === 1 ? elements[0] : elements;
-// };
-
-// // Toggle Navbar
-// $('#hamburger-menu').addEventListener('click', function (e) {
-//     $('nav .navbar-nav').classList.toggle('active');
-// });
-
-// // Toggle Search
-// $('#search').addEventListener('click', function (e) {
-//     $('nav .search-form').classList.toggle('active');
-//     $('#search-box').focus();
-//     e.preventDefault();
-// });
-
-// // Toggle Shopping Cart
-// $('#shopping-cart').addEventListener('click', function (e) {
-//     $('nav .shopping-cart').classList.toggle('active');
-//     e.preventDefault();
-// });
-
-// $('.product-detail').forEach((element) => {
-//     element.addEventListener('click', function (e) {
-//         e.preventDefault();
-//         $('#item-detail-modal').style.display = 'flex';
-//     });
-// });
-
-// $('.modal-close').addEventListener('click', function (e) {
-//     e.preventDefault();
-//     $('#item-detail-modal').style.display = 'none';
-// });
-
-// // Close Navbar, Search & Shopping Cart
-// document.addEventListener('click', function (e) {
-//     if (!e.target.matches('#hamburger-menu *, .navbar-nav *')) {
-//         $('nav .navbar-nav').classList.remove('active');
-//     }
-
-//     if (!e.target.matches('#search *, .search-form *')) {
-//         $('nav .search-form').classList.remove('active');
-//     }
-//     if (!e.target.matches('#shopping-cart *, .shopping-cart *')) {
-//         $('nav .shopping-cart').classList.remove('active');
-//     }
-//     if (e.target === $('#item-detail-modal')) {
-//         $('#item-detail-modal').style.display = 'none';
-//     }
-// });
